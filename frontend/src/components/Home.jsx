b7556ccb8647893318647c547074de661105b6c6
Puneet Upadhayay <puneetupx@gmail.com>
2025-01-08 12:10:51 +0530

    Add homepage and registration form

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
+import "../assets/home.css";
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Grants Matching Tool</h1>
+      <p>Your journey to finding the right grants starts here.</p>
+    </div>
+  );
+};
+
+export default Home;
